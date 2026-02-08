04faadc89ae13a7c8c2092ce00773cd1594d807f
Soufiane Maakouk <soufianemaakouk@gmail.com>
2026-02-08 02:15:07 +0000

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+const API = "https://YOUR-BACKEND.onrender.com";
+
+export const getItems = async () =>
+  (await fetch(`${API}/items`)).json();
+
+export const getFAs = async () =>
+  (await fetch(`${API}/functional-areas`)).json();
+
+export const getVenues = async () =>
+  (await fetch(`${API}/venues`)).json();
+
+export const getAllocations = async () =>
+  (await fetch(`${API}/allocations`)).json();
+
+export const createAllocation = async (data) =>
+  fetch(`${API}/allocations`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+export const getDeliveries = async () =>
+  (await fetch(`${API}/deliveries`)).json();
