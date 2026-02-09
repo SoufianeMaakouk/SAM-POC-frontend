ec79fc26b72b40d8622ed4d34d37ac63171753cc
Soufiane Maakouk <soufianemaakouk@gmail.com>
2026-02-09 23:18:34 +0000

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,13 @@
 const API = "https://sam-poc-backend.onrender.com";
 
-/* BASE */
-const get = url => fetch(url).then(r => r.json());
+/* ---------- HELPERS ---------- */
+const get = async (url) => {
+  const res = await fetch(url);
+  return res.json();
+};
 
-/* CATALOG */
-export const getItems = () => get(`${API}/items`);
-export const getFAs = () => get(`${API}/functional-areas`);
-export const getVenues = () => get(`${API}/venues`);
-export const getSubVenues = venueId =>
-  get(`${API}/sub-venues/${venueId}`);
-export const getSpaces = subVenueId =>
-  get(`${API}/spaces/${subVenueId}`);
-
-/* ALLOCATIONS */
-export const getAllocations = () => get(`${API}/allocations`);
-
-export const createAllocation = async data => {
-  const res = await fetch(`${API}/allocations`, {
+const post = async (url, data) => {
+  const res = await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
@@ -26,3 +17,40 @@ export const createAllocation = async data => {
   if (!res.ok) throw json;
   return json;
 };
+
+/* ---------- ITEMS ---------- */
+export const getItems = () => get(`${API}/items`);
+
+export const createItem = (data) =>
+  post(`${API}/items`, data);
+
+/* ---------- FUNCTIONAL AREAS ---------- */
+export const getFAs = () => get(`${API}/functional-areas`);
+
+export const createFA = (data) =>
+  post(`${API}/functional-areas`, data);
+
+/* ---------- VENUES ---------- */
+export const getVenues = () => get(`${API}/venues`);
+
+export const createVenue = (data) =>
+  post(`${API}/venues`, data);
+
+/* ---------- SUB VENUES ---------- */
+export const getSubVenues = (venueId) =>
+  get(`${API}/sub-venues/${venueId}`);
+
+/* ---------- SPACES ---------- */
+export const getSpaces = (subVenueId) =>
+  get(`${API}/spaces/${subVenueId}`);
+
+/* ---------- ALLOCATIONS ---------- */
+export const getAllocations = () =>
+  get(`${API}/allocations`);
+
+export const createAllocation = (data) =>
+  post(`${API}/allocations`, data);
+
+/* ---------- DELIVERIES ---------- */
+export const getDeliveries = () =>
+  get(`${API}/deliveries`);
