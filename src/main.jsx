8ae04b1d0f4dec564b3b14edb00d3ac136c51cf3
Soufiane Maakouk <soufianemaakouk@gmail.com>
2026-02-12 18:28:44 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
+import { AllocationProvider } from "./context/AllocationContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AllocationProvider>
+      <App />
+    </AllocationProvider>
   </React.StrictMode>
 );
